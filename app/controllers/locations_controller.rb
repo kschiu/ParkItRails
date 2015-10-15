32c1ddd55e533ad89a24fd3c8f84d33e4b81d65e
Raksha Rao <srir@andrew.cmu.edu>
2015-10-15 19:44:10 -0400

    Added photo parameter to controller in locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -69,6 +69,6 @@ class LocationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def location_params
-      params.require(:location).permit(:user_id, :street_address, :city, :state, :zip_code, :min_to_campus, :num_cars, :car_type)
+      params.require(:location).permit(:user_id, :street_address, :city, :state, :zip_code, :min_to_campus, :num_cars, :car_type, :photo)
     end
 end
