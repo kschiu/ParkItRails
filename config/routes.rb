13cf3deb12a761e4f4b57cdac939d479fe0a83ee
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-10-15 19:29:34 -0400

    Added login page and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,13 @@ Rails.application.routes.draw do
 
   resources :payments
 
+  resources :sessions
+
   # semi-static routes
   get 'home', to: 'home#index', as: :home
   get 'signup' => 'users#new', :as => :signup
   get 'login' => 'sessions#new', :as => :login
+  get 'logout' => 'sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
