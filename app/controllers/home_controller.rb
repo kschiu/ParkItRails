b2f270331f8a60cf312889a9f608a533ee141ff9
Swathi Anand <slanand32@gmail.com>
2015-10-15 20:56:24 -0400

    added photo field to form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 	def index
-		
+		@closestParkings = Listing.closest.chronological.active.paginate(:page => params[:page]).per_page(10)
   	
     end
+
 end
