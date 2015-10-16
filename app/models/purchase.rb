d780636e8146288c59b1c897c1cdcf369a3d5acd
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-10-15 20:33:43 -0400

    logic to make listing inactive after purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,14 @@
 class Purchase < ActiveRecord::Base
+	after_create :deactivate_listing
+
 	belongs_to :user
 	belongs_to :listing
 
 	validates_presence_of :date
 	validates_date :date
+
+	private
+	def deactivate_listing
+		self.listing.active = false
+	end
 end
